a51861fc56eb17542caaf46d047d15b32169ac81
Laura Feitosa <115425220+laurafeitosaa@users.noreply.github.com>
2024-07-15 22:15:10 -0300

    Update CRUDusuarios.sql

--- CRUDusuarios.sql
+++ CRUDusuarios.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `usuarios1` (
+CREATE TABLE `usuarios` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
   `fone` VARCHAR(45) NOT NULL,
   `data_nascimento` DATE NOT NULL,
   PRIMARY KEY (`id`));
-SELECT * FROM usuarios1; 
+SELECT * FROM usuarios; 
